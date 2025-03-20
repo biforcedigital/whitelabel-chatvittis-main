4fba573c8e094754a2251378eb7c458f0ac3e3ac
biforcedigital <biforcedigital@gmail.com>
2025-03-20 00:26:56 -0300

    Update app.ts - cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -45,8 +45,10 @@ app.set("queues", {
   sendScheduledMessages
 });
 
-const allowedOrigins = [process.env.FRONTEND_URL];
-
+const allowedOrigins = [
+  "https://newconnect.mss.tec.br",
+  "http://localhost:3000"
+];
 // Configuração do BullBoard
 if (String(process.env.BULL_BOARD).toLocaleLowerCase() === 'true' && process.env.REDIS_URI_ACK !== '') {
   BullBoard.setQueues(BullQueue.queues.map(queue => queue && queue.bull));
